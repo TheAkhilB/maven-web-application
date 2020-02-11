b231decfe317265b6171e4b8f81dd99f4b6b979a
theakhilb <theakhilkumarb@gmail.com>
2020-02-11 10:38:01 +0000

    added

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/employee")
 public class EmployeeService {
+	{
 
 	
 	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
